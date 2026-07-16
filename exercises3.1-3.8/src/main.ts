22723f37f4892e2b3889e2a335ecc3a4c6a3fc09
Viacheslav P <werbower@gmail.com>
2026-07-16 16:51:16 +0300

    exercise 3.3

--- exercises3.1-3.8/src/main.ts
+++ exercises3.1-3.8/src/main.ts
@@ -8,10 +8,20 @@ app.get('/api/persons', (req, res) => {
     res.json(state.persons)
 })
 
+app.get('/api/persons/:id', (req, res)=> {
+    const id = req.params.id
+    const person = state.persons.find(item=> item.id === id)
+    if (!person) 
+        return res.status(404).end()
+    res.send(person)
+})
+
 app.get('/info', (req, res)=> {
     res.send(`<div>Phone book has info for ${state.persons.length} people</div><br>
         <div>${(new Date()).toString()}</div>`)
 })
+
+
 
 const port = 3001
 app.listen(port, ()=> console.log(`app listening on port ${port}`))
